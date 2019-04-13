63c78933ccbdd28a883ab7661ba5a42254760ca0
Diaa Kasem <me@diaa.me>
2019-04-13 11:00:53 +0200

    Update game

--- game.jl
+++ game.jl
@@ -1,6 +1,6 @@
 const WAIT = 1
-const LIMIT = 100
-const TRIALS = 5
+const LIMIT = 1000
+const TRIALS = 10
 
 function intro()
     message("WELCOME TO GUESSES GAME")
@@ -15,7 +15,7 @@ function message(msg)
     printable = uppercase(msg)
     println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     println("+                                                                               ")
-    println("+                      $printable")
+    println("+             $printable")
     println("+                                                                               ")
     println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     run(`say $msg`)
@@ -27,8 +27,8 @@ end
 function getUserName()
     println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     println("+                                                                               ")
-    println("+                            WHAT IS YOUR NAME ?                                ")
-    println("+          ( 1- Mariam ) ( 2- Malak ) ( 3- Menna ) ( 4- Diaa )                  ")
+    println("+                   WHAT IS YOUR NAME ?                                ")
+    println("+  ( 1- Mariam ) ( 2- Malak ) ( 3- Menna ) ( 4- Diaa ) ( 5- Nawal )             ")
     println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     run(`say What is your name?`)
     name = readline()
@@ -37,9 +37,11 @@ function getUserName()
     elseif name == "2" || lowercase(name) == "malak"
         return "malak"
     elseif name == "3" || lowercase(name) == "menna"
-        return "mariam"
+        return "menna"
     elseif name == "4" || lowercase(name) == "diaa"
         return "diaa"
+    elseif name == "5" || lowercase(name) == "nawal"
+        return "nawal"
     else
         return getUserName()
     end
@@ -58,32 +60,51 @@ end
 intro()
 name = getUserName()
 welcome(name)
-message("ok, i will think of a number and you guess it.")
+message("I will think of a number between 1 and $LIMIT and you have $TRIALS trials to guess it.")
 guessedNumber = rand(1:LIMIT)
 message("ok, i have a number in my mind")
 message("what do you think the number is ?")
-function main(trials)
+
+global answers = []
+
+function main(trials, lastNum)
     if trials <= 0
         message("Ouch! You lose $name ! Sorry. ( The Number Was $guessedNumber )")
         return
     end
     num = parse(Int32, readline())
+    if num in answers
+        message("you have already said that number before.")
+        return main(trials, lastNum)
+    end
+    if guessedNumber > lastNum && num < lastNum
+        message("Whaat! Say a bigger number.")
+        return main(trials, lastNum)
+    end
+    if guessedNumber < lastNum && num > lastNum
+        message("Whaat! Say a smaller number.")
+        return main(trials, lastNum)
+    end
     function timesToGo(trials)
+        if trials > 2
+            return ""
+        end
         if trials-1 > 0
             return "[ $(trials-1) more times to go. ]"
         else
             return "[ GAME OVER ]"
         end
     end
+    push!(answers, num)
     if num > guessedNumber
         message("No, the number in my mind is smaller than $(num). $(timesToGo(trials))")
-        main(trials - 1)
+        main(trials - 1, num)
     elseif num < guessedNumber
         message("No, the number in my mind is bigger than $(num). $(timesToGo(trials))")
-        main(trials - 1)
+        main(trials - 1, num)
     else
-        message("Excellent! you win $name . woo hoo !!")
+        message("Excellent! you win $name in iteration number $trial . woo hoo !!")
         return
     end
 end
-main(TRIALS)
+main(TRIALS, -1)
